3a54be3cb8384d634851e11a05b3adaa72725a24
Max Strother <maxbax@op.pl>
2020-08-23 19:02:28 +0200

    Increase the width of the main component

--- components/NowPlaying.tsx
+++ components/NowPlaying.tsx
@@ -20,7 +20,7 @@ export const Player: React.FC<Props> = ({
   isPlaying,
 }) => {
   return (
-    <ReadmeImg width="256" height="64">
+    <ReadmeImg width="350" height="64">
       <style>
         {`
             .paused { 
